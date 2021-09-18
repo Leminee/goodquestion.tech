3c29d995d485356bf8404bced26e76b45ce4af68
Lemine <lemine.el.agheb1994@gmail.com>
2021-09-18 05:58:09 +0200

    first commit

---
+++ src/main/java/tech/goodquestion/GoodquestionApplication.java
@@ -0,0 +1,13 @@
+package tech.goodquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodquestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodquestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/goodquestion/GoodquestionApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.goodquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodquestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
