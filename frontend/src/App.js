9c1ced9b8df1002261f0d1bf890c06fbf5b6aca2
Lemine <lemine.el.agheb1994@gmail.com>
2021-12-24 08:06:42 +0100

    added axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+
+
+
+
+
+const Test1 = () => {
+
+  const Test2 = () =>{
+
+    axios.get("https://www.goodquestion.tech:8443/brockyou/api/v2/password").then(res =>{ 
+
+    console.log(res);
+    });
+ };
+
+ useEffect(()=>{ 
+   Test2();
+ },[]);
+ 
+ return <h1>Test</h1>;
+
+};
+
+
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Test1/>
     </div>
   );
 }
