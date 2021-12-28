481d4e27909d6e8ba9270ab747616bb22f341f9c
Lemine <lemine.el.agheb1994@gmail.com>
2021-12-29 00:14:12 +0100

    added serverdata requets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,78 @@
 import './App.css';
-import React, {useState, useEffect} from "react";
+import React, {useState,useEffect} from "react";
 import axios from 'axios';
 
 
+const ServerData = () => {
+
+   const [activeUserNow, getActiveUserNow] = useState([]);
+   const [activeUserRecord, getActiveUserRecord] = useState([]);
+   const [serverMemberAmount, getServerMemberAmount] = useState([]);
+   const [amountMessages, getAmountMessages] = useState([]);
+   const [newUserName, getNewUserName] = useState([]);
+   const [newUserJoiningTime, getNewUserJoiningTime] = useState([]);
 
 
+   const fetchServerData = () =>{
 
-const Test1 = () => {
 
-  const Test2 = () =>{
+   axios.get("http://localhost:5050/api/v1/active-user/now").then(res =>{
 
-    axios.get("https://www.goodquestion.tech:8443/brockyou/api/v2/password").then(res =>{ 
-
-    console.log(res);
+    getActiveUserNow(res.data);
     });
+
+      axios.get("http://localhost:5050/api/v1/active-user/record").then(res =>{
+
+         getActiveUserRecord(res.data);
+      });
+
+      axios.get("http://localhost:5050/api/v1/server-member/amount").then(res =>{
+
+          getServerMemberAmount(res.data);
+      });
+
+      axios.get("http://localhost:5050/api/v1/amount-messages/total").then(res =>{
+
+          getAmountMessages(res.data)
+      });
+
+      axios.get("http://localhost:5050/api/v1/new-user/name").then(res =>{
+
+          getNewUserName(res.data)
+      });
+
+      axios.get("http://localhost:5050/api/v1/new-user/time").then(res =>{
+
+          getNewUserJoiningTime(res.data)
+      });
+
  };
 
  useEffect(()=>{ 
-   Test2();
+   fetchServerData();
  },[]);
+
  
- return <h1>Test</h1>;
+ return (
+
+     <div>
+
+     <h3>Online</h3>
+     <p>{activeUserNow}</p>
+     <h3>Record</h3>
+     <p>{activeUserRecord}</p>
+     <h3>Servermitglieder</h3>
+     <p>{serverMemberAmount}</p>
+     <h3>Nachrichten</h3>
+     <p>{amountMessages}</p>
+     <h3>Neues Mitglied</h3>
+     <p>{newUserName}</p>
+     <h3>Gejoint am</h3>
+     <p>{newUserJoiningTime}</p>
+
+     </div>
+
+ )
 
 };
 
@@ -30,8 +82,7 @@ function App() {
   return (
 
     <div className="App">
-      
-      <Test1/>
+      <ServerData/>
     </div>
   );
 }
